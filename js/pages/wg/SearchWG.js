227b43d8f564787ef387d3f0d80432658c659d75
milld10 <c.reis@student.tugraz.at>
2016-05-23 21:00:00 +0200

    update of feature search wg

--- js/pages/wg/SearchWG.js
+++ js/pages/wg/SearchWG.js
@@ -16,14 +16,32 @@ export default class SearchWG extends Component {
   constructor(props)
   {
     super(props);
+
+    this.state = {searchterm: ""}
   }
 
+  textchangehandler(text)
+  {
+    console.log(text);
+    this.setState({searchterm: text})
+    const requesturl = `http://10.0.2.2:1337/parse/classes/wgs?where={"name": {"$regex":".*${text}.*"}}`
+    console.log(requesturl)
+      fetch( requesturl,
+      { method: "GET",
+        headers: { "X-Parse-Application-Id": "StudentWGPlanner" },
+      })
+      .then((res) => res.json())
+      .then((response) => {console.log(response)})
+      .catch((error) => {console.log(error)})
+
+  }
 
   render()
   {
     return (
       <View>
-        <Text>Todo implement Search WG</Text>
+        <TextInput onChangeText={this.textchangehandler.bind(this)}
+        value={this.state.searchterm}></TextInput>
       </View>
     );
   }
