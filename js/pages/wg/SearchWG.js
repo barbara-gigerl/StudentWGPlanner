3f7290ac9c2ccbfc887944fad1c0279a40eb8b97
milld10 <c.reis@student.tugraz.at>
2016-05-25 09:01:15 +0200

    search wg is working

--- js/pages/wg/SearchWG.js
+++ js/pages/wg/SearchWG.js
@@ -7,9 +7,14 @@ import React, {
   TextInput,
   View,
   TouchableHighlight,
+  ListView,
 } from 'react-native';
 
+import Parse from "parse/react-native"
 
+Parse.initialize("StudentWGPlanner")
+Parse.serverURL="http://10.0.2.2:1337/parse"
+const WGObject = Parse.Object.extend("wgs")
 
 export default class SearchWG extends Component {
 
@@ -17,23 +22,28 @@ export default class SearchWG extends Component {
   {
     super(props);
 
-    this.state = {searchterm: ""}
+    this.state = {searchterm: "",
+      wgs: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1.id !== r2.id })
+    }
   }
 
   textchangehandler(text)
   {
     console.log(text);
-    this.setState({searchterm: text})
-    const requesturl = `http://10.0.2.2:1337/parse/classes/wgs?where={"name": {"$regex":".*${text}.*"}}`
-    console.log(requesturl)
-      fetch( requesturl,
-      { method: "GET",
-        headers: { "X-Parse-Application-Id": "StudentWGPlanner" },
-      })
-      .then((res) => res.json())
-      .then((response) => {console.log(response)})
-      .catch((error) => {console.log(error)})
 
+    let query = new Parse.Query(WGObject)
+
+    this.setState({searchterm: text})
+    query.matches("name", new RegExp(`${text}`, "ig"));
+    query.find().then((results) => {
+        this.setState({wgs: this.state.wgs.cloneWithRows([...results])})
+    })
+    .catch((error) => {console.log(error)})
+  }
+
+  renderWg(wg)
+  {
+    return (<Text>{wg.get("name")}</Text>)
   }
 
   render()
@@ -42,6 +52,8 @@ export default class SearchWG extends Component {
       <View>
         <TextInput onChangeText={this.textchangehandler.bind(this)}
         value={this.state.searchterm}></TextInput>
+        <ListView dataSource={this.state.wgs}
+          renderRow={this.renderWg.bind(this)} />
       </View>
     );
   }
