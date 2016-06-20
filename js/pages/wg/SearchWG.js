6e826ee9ebbef771811cad9e950e4e502ef1cd26
Sandra Brettschuh <brettschuh@student.tugraz.at>
2016-06-20 22:18:03 +0200

    [MW,SB] small changes

--- js/pages/wg/SearchWG.js
+++ js/pages/wg/SearchWG.js
@@ -101,9 +101,17 @@ export default class SearchWG extends Component {
         console.log(response);
 
         if (response.data.results.length === 1)
+        {
           this.insertDatabase(response.data.results[0]);
+        }
+        else
+        {
+          this.setState({errormessage: "Couldn't find wg"});
+        }
+
         })
       .catch((error) => {
+
         console.log(error);
         this.setState({errormessage: "Couldn't connect to server."});
       });
@@ -115,7 +123,7 @@ export default class SearchWG extends Component {
   insertDatabase(resultObject)
   {
     for (var i = 0; i < resultObject.users.length; i++) {
-      if (resultObject.users[i] === GLOBAL.USERID) {
+      if (resultObject.users[i].id === GLOBAL.USERID) {
         this.setState({errormessage: 'You are already a member of this WG.'});
         return true;
       }
@@ -131,6 +139,9 @@ export default class SearchWG extends Component {
       headers: config.PARSE_SERVER_HEADERS
     }).then(response => {
       console.log(response)
+      GLOBAL.WGID = resultObject.objectId
+      this.props.navigator.push({
+         name: "Home"});
     }).catch(function(error) {
       this.setState({errormessage: "Couldn't connect to server."});
       console.log(error);
